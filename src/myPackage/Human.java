512ae4134e559fe886ff4f46d569823471d53eee
Thang Viet Tran <thangtv1106@gmail.com>
2017-03-04 08:32:20 +0700

    Init

---
+++ src/myPackage/Human.java
@@ -0,0 +1,43 @@
+package myPackage;
+
+public class Human {
+	private String name;
+	private byte age;
+	private String addr;
+
+	public Human() {
+		super();
+	}
+
+	public Human(String name, byte age, String addr) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.addr = addr;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public byte getAge() {
+		return age;
+	}
+
+	public void setAge(byte age) {
+		this.age = age;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+}
